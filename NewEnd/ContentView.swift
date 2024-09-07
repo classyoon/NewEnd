c253a8087edc1b1366c0bf0811c8ec314aab5886
classyoon <96508736+classyoon@users.noreply.github.com>
2024-09-07 11:29:46 -0400

    Marvelous sucess

--- NewEnd/ContentView.swift
+++ NewEnd/ContentView.swift
@@ -6,56 +6,84 @@
 //
 
 import SwiftUI
-import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-
+    @ObservedObject var vm : EndVM
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
+        VStack {
+            Text(vm.endingText)
+            Button(vm.endingButton){
+                vm.setEnd()
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
+            
         }
     }
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
-        }
-    }
+    
 }
 
 #Preview {
-    ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
+    ContentView(vm: EndVM())
+}
+enum EndingType {
+    case first, second, third
+}
+class EndDecider {
+    var end : EndingType
+    
+    func endOne(){
+        print("1")
+    }
+    func endTwo(){
+        print("2")
+    }
+    func endThree(){
+        print("3")
+    }
+    func rotate(){
+        switch end {
+        case .first :
+            end = .second
+            print("Rotate 1")
+        case .second :
+            end = .third
+            print("Rotate 2")
+        case .third :
+            end = .first
+            print("Rotate 3")
+        }
+    }
+    
+    init(end: EndingType = .first) {
+        self.end = end
+    }
+}
+class EndVM : ObservableObject{
+    var model : EndDecider = EndDecider()
+    @Published var endingText : String = "nothing"
+    @Published var endingButton : String = "begin"
+    @Published var buttonAction : ()
+    
+    func setEnd(){
+        rotate()
+        switch model.end {
+        case .first:
+            buttonAction = model.endOne()
+            endingText = "Ending 1, tap to move to ending 2"
+            endingButton = "Ending 2"
+        case .second:
+            buttonAction = model.endTwo()
+            endingText = "Ending 2, tap to move to ending 3"
+            endingButton = "Ending 3"
+        case .third:
+            buttonAction = model.endThree()
+            endingText = "Ending 3, tap to move to ending 1"
+            endingButton = "Ending 1"
+        }
+        
+    }
+    func rotate(){
+        model.rotate()
+    }
+    
 }
